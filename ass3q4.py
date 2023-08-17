2ca04644f9f53a80bbb84d7ff2a8f5bf2ee1b5bf
harsh1832004 <142489897+harsh1832004@users.noreply.github.com>
2023-08-17 15:46:37 +0530

    Add files via upload

---
+++ ass3q4.py
@@ -0,0 +1,22 @@
+def calculateSalary(basic, grade):
+     
+    hra = 0.2 * basic
+    da = 0.5 * basic
+    pf = 0.11 * basic
+     
+    if grade == 'A':
+        allowance = 1700.0
+    elif grade == 'B':
+        allowance = 1500.0
+    else:
+        allowance = 1300.0;
+     
+    gross = round(basic + hra + da + allowance - pf)
+                     
+    return gross
+
+basic = int(input("enter the basic salary: "))
+grade = input("enter the grade: ")
+    
+print(calculateSalary(basic, grade));
+
